9b9caf993b5f1cad5bbcce3f8f3254b91e9ace41
mingyo <alsry2325@naver.com>
2025-11-08 18:59:09 +0900

    모듈과 파일처리 실습 커밋

--- 새싹_모듈과파일처리/student.py
+++ 새싹_모듈과파일처리/student.py
@@ -2,30 +2,28 @@
 # 함수들을 정의하세요.
 students = []
 def add_student(name, age, grade):
-    keys = {}
-    # students.append(name,age,grade)
-    # print(students)
-    keys["name"] = name
-    keys["age"] = age
-    keys["grade"] = grade
-
-    print(keys)
-    # students = keys
-    students.append(keys)
+    # keys = {}
+    # # students.append(name,age,grade)
+    # # print(students)
+    # keys["name"] = name
+    # keys["age"] = age
+    # keys["grade"] = grade
+    # print(keys)
+    # students.append(keys)
     # print(students)
     # print("추가 완료")
-    
+    students.append({'name' : name, 'age' : age, 'grade' : grade})
 def get_all_students():
     print("전체 학생 목록")
     for i in students:
         print(i)
     
 def find_student(name):
-    # print(name)
+
     # [{'name': '정민교1', 'age': 20, 'grade': 'a'}, {'name': '정민교2', 'age': 21, 'grade': 5}, {'name': '정민교3', 'age': 22, 'grade': 4}]
-    for i in students:
-        if  i["name"] == name:
-            print(f"학생 검색 :{name} /n {i}")
+    for student in students:
+        if  student["name"] == name:
+            return student
     
 def get_average_age():
     total = 0 
